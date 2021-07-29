25e766bd7c0120e441c8da20c39b3729c0ec92a3
Wytamma Wirth <wytamma.wirth@me.com>
2021-07-29 11:20:04 +1000

    :art: black

--- dynamic_beast/main.py
+++ dynamic_beast/main.py
@@ -6,41 +6,46 @@ from xml.dom import minidom
 
 app = typer.Typer()
 
+
 def make_dynamic(element, key, parameter=False):
-    id = element.get('id').split('.')[0]
+    id = element.get("id").split(".")[0]
     if key is None:
-      value = element.text
-      s = f"{id}={value}"
-      element.text = f"$({s})"
+        value = element.text
+        s = f"{id}={value}"
+        element.text = f"$({s})"
     else:
-      value = element.get(key)
-      if '$' in value:
-        value = value.replace('$', '\$')
-      if '=' in value:
-        # https://github.com/CompEvol/beast2/blob/4d95f0095d20a6999d2757299c629bfbbf14b94b/src/beast/util/XMLParser.java#L372
-        # the way that defaults are passed means you can't use '=' in there values... 
-        # https://github.com/CompEvol/beast2/pull/991
-        return None 
-      s = f"{id}.{key}={value}"
-      element.set(key, f"$({s})")
+        value = element.get(key)
+        if "$" in value:
+            value = value.replace("$", "\$")
+        if "=" in value:
+            # https://github.com/CompEvol/beast2/blob/4d95f0095d20a6999d2757299c629bfbbf14b94b/src/beast/util/XMLParser.java#L372
+            # the way that defaults are passed means you can't use '=' in there values...
+            # https://github.com/CompEvol/beast2/pull/991
+            return None
+        s = f"{id}.{key}={value}"
+        element.set(key, f"$({s})")
+
 
 def make_all_dynamic(element):
-    for key in filter(lambda k: k != 'id', element.keys()):
-      make_dynamic(element, key)
-    if element.tag == 'parameter': 
-      make_dynamic(element, None, True)
+    for key in filter(lambda k: k != "id", element.keys()):
+        make_dynamic(element, key)
+    if element.tag == "parameter":
+        make_dynamic(element, None, True)
 
 
 @app.command()
-def main(beast_xml:Path, outfile: Path = typer.Option(None, help="Path to save the dynamic BEAST XML file."),):
+def main(
+    beast_xml: Path,
+    outfile: Path = typer.Option(None, help="Path to save the dynamic BEAST XML file."),
+):
     """
     Dynamic BEAST XML
     """
     tree = ET.parse(beast_xml)
     root = tree.getroot()
-    run = root.find('run')
+    run = root.find("run")
     for el in run.iter():
-        if 'idref' in el.keys():
+        if "idref" in el.keys():
             continue
         make_all_dynamic(el)
     if outfile:
@@ -48,4 +53,3 @@ def main(beast_xml:Path, outfile: Path = typer.Option(None, help="Path to save t
     else:
         xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(newl="")
         typer.echo(xmlstr)
-
--- tests/test_dynamic_beast.py
+++ tests/test_dynamic_beast.py
@@ -2,14 +2,17 @@ from dynamic_beast import __version__, app
 from typer.testing import CliRunner
 import xml.etree.ElementTree as ET
 
+
 def test_version():
-    assert __version__ == '1.1.0'
+    assert __version__ == "1.1.0"
+
 
 runner = CliRunner()
+
 
 def test_app():
     result = runner.invoke(app, ["data/BEAST.xml"])
     assert result.exit_code == 0
     parsed = ET.fromstring(str(result.stdout))
-    expected = ET.parse('data/dynamic_BEAST.xml')
+    expected = ET.parse("data/dynamic_BEAST.xml")
     assert ET.tostring(parsed) == ET.tostring(expected.getroot())
